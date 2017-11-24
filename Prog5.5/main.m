c24fd79df3e600e1b3a26f8ed8ab814ab56e595f
admin <dangtinhtong@gmail.com>
2017-11-24 23:28:33 +0700

    first commit

--- Prog5.5/main.m
+++ Prog5.5/main.m
@@ -2,16 +2,33 @@
 //  main.m
 //  Prog5.5
 //
-//  Created by admin on 11/24/17.
-//  Copyright © 2017 admin. All rights reserved.
+//  Created by Tống Đăng Tình on 11/24/17.
+//  Copyright © 2017 Tống Đăng Tình. All rights reserved.
 //
-
+// Nested for loops
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        
+        int  n, number, triangularNumber, counter;
+        for(counter=1;counter<=5;++counter){
+            NSLog(@"What triangular number do you want?");
+            scanf("%i",&number) ;
+        
+        
+        
+        triangularNumber=0;
+        
+        
+        for(n=1; n<= number; ++n)
+            triangularNumber+=n;
+        NSLog(@"Triangular number %i is %i",number,triangularNumber);
+            
     }
+    
     return 0;
+    }
 }
